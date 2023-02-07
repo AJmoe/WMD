192ddc39eff8decc8b86d4b9c976b0d729078b6e
Aobakwe Moerane <cse22-063@thuto.bac.ac.bw>
2023-02-07 11:54:13 +0200

    changed title

---
+++ wmd.java
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<Head>
+<title><b> WELCOME TO JAVA TUTORIAL</b></title>
+</Head>
+<body>
+
+<h1><b>HISTORY OF JAVA</b> </H1>
+
+<p1>The history of Java is very interesting.Java was -<b>originally
+ designed for interactive television</b>, but it was too advanced 
+ technology for the digital cable television industry at the time. 
+ The history of Java starts with the Green Team. Java team members
+ (also known as Green Team), initiated this project to develop a 
+ language for digital devices such as set-top boxes, televisions, 
+ etc. However, it was best suited for internet programming.Later,
+ Java technology was incorporated by Netscape.</p>
+ 
+ <h2><b> USINGS OF JAVA</b></h2>
+  
+ 
+ 
+ 
